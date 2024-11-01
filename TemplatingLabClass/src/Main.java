e7ca32d37a6c0b68302e02c9d05010061602f0c3
Gilbert Isaac Salazar <salazarg435@gmail.com>
2024-11-01 13:45:52 -0500

    Update
    Main was made into functions and added try catch blocks as well as fixed up classes and member variable names

--- TemplatingLabClass/src/Main.java
+++ TemplatingLabClass/src/Main.java
@@ -7,85 +7,134 @@ interface Ingredient{
 
 class SolidIngredient implements Ingredient{
     private String _ingredient;
-    private double _quantity;
+    private double _quantityInGrams;
     public SolidIngredient(String i, double q){
         _ingredient = i;
-        _quantity = q;
+        _quantityInGrams = q;
     }
     public String getName(){return _ingredient;}
-    public double getQuantity(){return _quantity;}
+    public double getQuantity(){return _quantityInGrams;}
 }
 
 class LiquidIngredient implements Ingredient{
     private String _ingredient;
-    private double _quantity;
+    private double _quantityInML;
     public LiquidIngredient(String i, double q){
         _ingredient = i;
-        _quantity = q;
+        _quantityInML = q;
     }
     public String getName(){return _ingredient;}
-    public double getQuantity(){return _quantity;}
+    public double getQuantity(){return _quantityInML;}
 }
 
 class Recipe<T extends Ingredient>{
     private String _name;
     private String _instructions;
     private ArrayList<T> _ingredients;
-    public Recipe(String name, String instructions, ArrayList<T> ingredients){
+    public Recipe(String name, String instructions){
         _name = name;
         _instructions = instructions;
-        _ingredients = ingredients;
+        _ingredients = new ArrayList<>();
     }
     public void addIngredient(T t){
         _ingredients.add(t);
     }
     public void print(){
-        System.out.println(_name);
-        System.out.println(_instructions);
-        System.out.println(_ingredients);
+        System.out.println("Recipe: " + _name);
+        System.out.println("Instructions: " + _instructions);
+        System.out.println("Ingredients: ");
+        for(T t: _ingredients){
+            System.out.println("- " + t.getName() + ": " + t.getQuantity());
+        }
+        System.out.println("___________________________________________________");
     }
 
 }
 
 public class Main {
-    public static void main(String[] args) {
-
-        SolidIngredient si = new SolidIngredient("flour", 3);
-        LiquidIngredient li = new LiquidIngredient("oil", 2);
-        Recipe<Ingredient> r = new Recipe<>("flour oil", "Add flour and oil together", new ArrayList<>());
-        r.addIngredient(si);
-        r.addIngredient(li);
-
-
-
-        int choice;
-        Scanner scan = new Scanner(System.in);
+    public static int menu(Scanner scan){
+        int choice = 0;
         System.out.println("1. Add Ingredient");
         System.out.println("2. Show Ingredient List");
         System.out.println("3. Quit");
-        choice = scan.nextInt();
-        while(choice > 3 || choice < 0){
-            System.out.println("Invalid, try again");
-            choice = scan.nextInt();
-        }
-        while(choice != 3){
-            if (choice == 1){
-                r.addIngredient(li);
-            }
-            else{
-                r.print();
-            }
-
-            System.out.println("1. Add Ingredient");
-            System.out.println("2. Show Ingredient List");
-            System.out.println("3. Quit");
-            choice = scan.nextInt();
+        try {
+            choice = Integer.parseInt(scan.nextLine());
             while(choice > 3 || choice < 0){
                 System.out.println("Invalid, try again");
-                choice = scan.nextInt();
+                choice = Integer.parseInt(scan.nextLine());
             }
-
         }
-        System.out.println("goodbye");
+        catch(InputMismatchException e){
+            System.out.println("Invalid data entered");
+            System.exit(-1);
+        }
+        return choice;
+    }
+
+    public static void addIngredient(Recipe<Ingredient> recipe, Scanner scan){
+        String name;
+        String dryOrLiquid;
+        double quantity = 0;
+
+        System.out.println("Dry or Liquid: (d/l)");
+        dryOrLiquid = scan.nextLine();
+        while(dryOrLiquid.length() != 1){
+            System.out.println("Error, must be one character");
+            System.out.println("Dry or Liquid: (d/l) ");
+            dryOrLiquid = scan.nextLine();
+        }
+
+        System.out.println("Name of ingredient: ");
+        name = scan.nextLine();
+
+        System.out.println("Quantity: ");
+        try{
+            quantity = Double.parseDouble(scan.nextLine());
+        }
+        catch(NumberFormatException e){
+            System.out.println("Error");
+            System.exit(-1);
+        }
+
+        dryOrLiquid = dryOrLiquid.toLowerCase();
+        Ingredient ingredient;
+        if (dryOrLiquid.charAt(0) == 'd') {
+            ingredient = new SolidIngredient(name, quantity);
+            recipe.addIngredient(ingredient);
+            System.out.println("Added Dry ingredient");
+        }
+
+        else if (dryOrLiquid.charAt(0) == 'l') {
+            ingredient = new LiquidIngredient(name, quantity);
+            recipe.addIngredient(ingredient);
+            System.out.println("Added Dry ingredient");
+        }
+        else {
+            System.out.println("Error");
+        }
+    }
+
+    public static void main(String[] args) {
+        Recipe<Ingredient> recipe;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Recipe name: ");
+        String name = scan.nextLine();
+
+        System.out.println("Instructions: ");
+        String instructions = scan.nextLine();
+        
+        recipe = new Recipe<>(name, instructions);
+        int choice = menu(scan);
+
+        while(choice != 3) {
+            if (choice == 1)
+                addIngredient(recipe, scan);
+            else
+                recipe.print();
+
+            choice = menu(scan);
+        }
+        System.out.println("Goodbye");
     }
 }
